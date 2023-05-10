118ff2483cdb42d21ec6f17faad12b231a2bbe6a
Holger Bohlmann <holger.bohlmann@it-agile.de>
2023-05-10 15:05:14 +0200

    Letztes Entfernen von Folge-OP

--- src/main/java/de/itagile/golf/operation/Lochwechsel.java
+++ src/main/java/de/itagile/golf/operation/Lochwechsel.java
@@ -5,19 +5,9 @@ import de.itagile.golf.Scorecard;
 
 public final class Lochwechsel implements Operation {
 	
-	private final Operation folgeoperation;
-
-	public Lochwechsel() {
-		this.folgeoperation = null;
-	}
-
-	public Lochwechsel(Operation folgeoperation) {
-		this.folgeoperation = folgeoperation;
-	}
-
 	public String fuehreAus(Scorecard scorecard) {
 		scorecard.schliesseLochAb();
 		
-		return folgeoperation == null ? "" : folgeoperation.fuehreAus(scorecard);
+		return "";
 	}
 }
--- src/unittests/java/de/itagile/golf/operation/LochwechselTest.java
+++ src/unittests/java/de/itagile/golf/operation/LochwechselTest.java
@@ -1,16 +1,11 @@
 package de.itagile.golf.operation;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import de.itagile.golf.Operation;
 import de.itagile.golf.Scorecard;
 
 
@@ -18,25 +13,16 @@ public class LochwechselTest {
 	
 	private Scorecard scorecard;
 	private Lochwechsel lochwechsel;
-	private Operation folgeOperation;
 
 	@BeforeEach
 	public void setup() {
 		scorecard = mock(Scorecard.class);
-		folgeOperation = mock(Operation.class);
-		lochwechsel = new Lochwechsel(folgeOperation);		
+		lochwechsel = new Lochwechsel();		
 	}
 	
 	@Test
 	public void setztAnzahlSchlaegeZurueck() throws Exception {
 		lochwechsel.fuehreAus(scorecard);
 		verify(scorecard).schliesseLochAb();
-	}
-	
-	@Test
-	public void gibtAusgabeDerFolgeoperationMitAus() throws Exception {
-		when(folgeOperation.fuehreAus(any(Scorecard.class))).thenReturn("folgeoperationAusgabe");
-		
-		assertThat(lochwechsel.fuehreAus(scorecard), is("folgeoperationAusgabe"));	
 	}
 }
