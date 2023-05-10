415cc2a1bf77737fce45ada8849b2b0541b0fba7
Catharine Krämer <catharine.kraemer@mac-its.com>
2023-05-10 09:58:13 +0200

    scorecard befehl

--- src/main/java/de/itagile/golf/operation/ScorecardAusgabe.java
+++ src/main/java/de/itagile/golf/operation/ScorecardAusgabe.java
@@ -1,14 +1,25 @@
 package de.itagile.golf.operation;
 
+import java.util.Iterator;
+
 import de.itagile.golf.Operation;
 import de.itagile.golf.Scorecard;
+import de.itagile.golf.util.SystemProperties;
 
 public class ScorecardAusgabe implements Operation {
 
 	@Override
 	public String fuehreAus(Scorecard scorecard) {
-		// TODO Auto-generated method stub
-		return "Du hast noch nicht gespielt.";
+		String lochausgabe = "";
+		
+		if (scorecard.lochPunkte().isEmpty()) {
+			lochausgabe = "Du hast noch nicht gespielt.";
+		} else {
+			for (int i = 1; i <= scorecard.lochPunkte().size(); i++) {
+				lochausgabe += i + ". Loch: " + scorecard.lochPunkte().get(i) + SystemProperties.LINE_SEPARATOR;
+			}
+		}
+		return lochausgabe.trim();
 	}
 
 }
--- src/unittests/java/de/itagile/golf/operation/ScorecardTest.java
+++ src/unittests/java/de/itagile/golf/operation/ScorecardTest.java
@@ -7,10 +7,13 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import java.util.Collections;
+import java.util.Map;
+import java.util.HashMap;
 
 import org.junit.jupiter.api.Test;
 
 import de.itagile.golf.Scorecard;
+import de.itagile.golf.util.SystemProperties;
 
 class ScorecardTest {
 
@@ -21,4 +24,22 @@ class ScorecardTest {
 		assertThat(new ScorecardAusgabe().fuehreAus(scorecard ), containsString("Du hast noch nicht gespielt."));
 	}
 
+	@Test
+	void gibtScorecardAus() {
+		Map map = new HashMap<Integer, Integer>();
+		map.put(1, 1);
+		Scorecard scorecard = mock(Scorecard.class);
+		when(scorecard.lochPunkte()).thenReturn(map);
+		assertThat(new ScorecardAusgabe().fuehreAus(scorecard ), containsString("1. Loch: 1"));
+	}
+	
+	@Test
+	void gibtScorecardAusMehrereZeilen() {
+		Map map = new HashMap<Integer, Integer>();
+		map.put(1, 1);
+		map.put(2, 1);
+		Scorecard scorecard = mock(Scorecard.class);
+		when(scorecard.lochPunkte()).thenReturn(map);
+		assertThat(new ScorecardAusgabe().fuehreAus(scorecard ), containsString("1. Loch: 1" + SystemProperties.LINE_SEPARATOR + "2. Loch: 1"));
+	}
 }
