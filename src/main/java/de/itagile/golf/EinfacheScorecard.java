bd0ad6171e024676e586a94de7a585420eaf247e
gruber <gruber@mbp-von-koray.fritz.box>
2023-05-10 09:39:16 +0200

    EinfacheScorecard erweitert und UnitTest hinzugefügt

--- src/main/java/de/itagile/golf/EinfacheScorecard.java
+++ src/main/java/de/itagile/golf/EinfacheScorecard.java
@@ -2,19 +2,27 @@ package de.itagile.golf;
 
 import javax.naming.OperationNotSupportedException;
 import java.util.Map;
+import java.util.HashMap;
 
 public class EinfacheScorecard implements Scorecard {
 
 	private int aktuellesLoch = 1;
 	private int anzahlSchlaege;
+	private Map<Integer, Integer> map = new HashMap<Integer, Integer>();
 
+	public EinfacheScorecard() {
+		map.put(1, 0);
+	}
+	
 	public void erhoeheAnzahlSchlaege() {
 		anzahlSchlaege++;
+		map.put(aktuellesLoch, anzahlSchlaege);
 	}
 
 	public void schliesseLochAb() {
 		aktuellesLoch++;
 		anzahlSchlaege = 0;
+		map.put(aktuellesLoch, anzahlSchlaege);
 	}
 
 	public int anzahlSchlaege() {
@@ -26,6 +34,7 @@ public class EinfacheScorecard implements Scorecard {
 	}
 
 	public Map<Integer, Integer> lochPunkte() {
-		throw new RuntimeException("Nich da!");
+		return map;
 	}
+	
 }
--- src/unittests/java/de/itagile/golf/EinfacheScorecardTest.java
+++ src/unittests/java/de/itagile/golf/EinfacheScorecardTest.java
@@ -2,6 +2,8 @@ package de.itagile.golf;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.jupiter.api.Test;
 
@@ -40,4 +42,56 @@ public class EinfacheScorecardTest {
 		scorecard.schliesseLochAb();
 		assertThat(scorecard.aktuellesLoch(), is(2));
 	}
+	
+	@Test
+	public void lochPunkte() throws Exception {
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		map.put(1, 0);
+		assertThat(scorecard.lochPunkte(), is(map));
+	}
+
+	@Test
+	public void lochPunktenNachEinemSchlag() throws Exception {
+		scorecard.erhoeheAnzahlSchlaege();
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		map.put(1, 1);
+		assertThat(scorecard.lochPunkte(), is(map));
+	}
+	
+	@Test
+	public void lochPunktenNachMehrerenSchlaegen() throws Exception {
+		scorecard.erhoeheAnzahlSchlaege();
+		scorecard.erhoeheAnzahlSchlaege();
+		scorecard.erhoeheAnzahlSchlaege();
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		map.put(1, 3);
+		assertThat(scorecard.lochPunkte(), is(map));
+	}
+		
+	@Test
+	public void lochPunktenBeimZweitenLoch() throws Exception {
+		scorecard.schliesseLochAb();
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		map.put(1, 0);
+		map.put(2, 0);
+		assertThat(scorecard.lochPunkte(), is(map));
+	}
+	
+	@Test
+	public void lochPunktenBeimDrittenLochUndMehrerenSchlaegen() throws Exception {
+		scorecard.erhoeheAnzahlSchlaege();
+		scorecard.schliesseLochAb();
+		scorecard.erhoeheAnzahlSchlaege();
+		scorecard.erhoeheAnzahlSchlaege();
+		scorecard.schliesseLochAb();
+		scorecard.erhoeheAnzahlSchlaege();
+		scorecard.erhoeheAnzahlSchlaege();
+		scorecard.erhoeheAnzahlSchlaege();
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		map.put(1, 1);
+		map.put(2, 2);
+		map.put(3, 3);
+		assertThat(scorecard.lochPunkte(), is(map));
+	}
+
 }
